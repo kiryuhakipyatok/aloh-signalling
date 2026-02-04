52ae15e8e6bc1e3628da7b445877a26afaba95df
Kiryl Chaplinski <goodapple5207@gmail.com>
2026-02-04 18:47:34 +0300

    fix(raw json)

--- internal/protocols/errs.go
+++ internal/protocols/errs.go
@@ -38,49 +38,49 @@ func NewResponseMessage(mid string, code uint, payload []byte) ResponseMessage {
 
 func StreamErrorMessage(mid string) ([]byte, error) {
 	op := "protocols.StreamErrorMessage"
-	em := NewResponseMessage(mid, STREAM_ERROR, []byte{})
+	em := NewResponseMessage(mid, STREAM_ERROR, json.RawMessage([]byte{}))
 	return marshalResponse(op, em)
 }
 
 func InternalServerErrorMessage(mid string) ([]byte, error) {
 	op := "protocols.InternalServerErrorMessage"
-	em := NewResponseMessage(mid, INTERNAL_ERROR, []byte{})
+	em := NewResponseMessage(mid, INTERNAL_ERROR, json.RawMessage([]byte{}))
 	return marshalResponse(op, em)
 }
 
 func InvalidProtocolErrorMessage(mid string) ([]byte, error) {
 	op := "protocols.InvalidProtocolErrorMessage"
-	em := NewResponseMessage(mid, INVALID_PROTOCOL, []byte{})
+	em := NewResponseMessage(mid, INVALID_PROTOCOL, json.RawMessage([]byte{}))
 	return marshalResponse(op, em)
 }
 
 func InvalidTypeErrorMessage(mid string) ([]byte, error) {
 	op := "protocols.InvalidTypeErrorMessage"
-	em := NewResponseMessage(mid, INVALID_TYPE, []byte{})
+	em := NewResponseMessage(mid, INVALID_TYPE, json.RawMessage([]byte{}))
 	return marshalResponse(op, em)
 }
 
 func ErrorNotFoundMessage(mid string) ([]byte, error) {
 	op := "protocols.ErrorNotFoundMessage"
-	em := NewResponseMessage(mid, NOT_FOUND, []byte{})
+	em := NewResponseMessage(mid, NOT_FOUND, json.RawMessage([]byte{}))
 	return marshalResponse(op, em)
 }
 
 func ErrorAlreadyExistsMessage(mid string) ([]byte, error) {
 	op := "protocols.ErrorAlreadyExistsMessage"
-	em := NewResponseMessage(mid, ALREADY_EXISTS, []byte{})
+	em := NewResponseMessage(mid, ALREADY_EXISTS, json.RawMessage([]byte{}))
 	return marshalResponse(op, em)
 }
 
 func ErrorRequestTimeoutMessage(mid string) ([]byte, error) {
 	op := "protocols.ErrorRequestTimeoutMessage"
-	em := NewResponseMessage(mid, REQUEST_TIMEOUT, []byte{})
+	em := NewResponseMessage(mid, REQUEST_TIMEOUT, json.RawMessage([]byte{}))
 	return marshalResponse(op, em)
 }
 
 func SuccessMessage(mid string) ([]byte, error) {
 	op := "protocols.SuccessMessage"
-	sm := NewResponseMessage(mid, SUCCESS, []byte{})
+	sm := NewResponseMessage(mid, SUCCESS, json.RawMessage([]byte{}))
 	return marshalResponse(op, sm)
 }
 
