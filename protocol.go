0bc18b1de05201130441e133cafbc9468c06a6e9
Kiryl Chaplinski <goodapple5207@gmail.com>
2026-07-04 15:48:37 +0300

    proto fix

--- protocol.go
+++ protocol.go
@@ -4,7 +4,7 @@ import "github.com/kiryuhakipyatok/aloh-signalling/internal/protocols"
 
 type (
 	Message                = protocols.Message
-	UserId                 = protocols.Message
+	UserId                 = protocols.UserId
 	SendPayloadMessage     = protocols.SendPayloadMessage
 	DatagramProxingMessage = protocols.DatagramProxingMessage
 	CredsMessage           = protocols.CredsMessage
